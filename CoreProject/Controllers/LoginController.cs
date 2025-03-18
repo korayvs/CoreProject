cffb9cfb98b55d05ba99d3e65b95d16bd7edaa3b
Koray Vatansever <koray-vatansever@hotmail.com>
2025-03-18 19:20:12 +0300

    end

--- CoreProject/Controllers/LoginController.cs
+++ CoreProject/Controllers/LoginController.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreProject.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CoreProject.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        Context c = new Context();
+
+        [HttpGet]
+        public IActionResult Signin()
         {
             return View();
         }
 
-        public IActionResult Signin()
+        public async Task<IActionResult> Signin(Admin p)
         {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == p.Kullanici && x.Sifre == p.Sifre);
+            if (bilgiler != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, p.Kullanici)
+                };
+                var useridentity = new ClaimsIdentity(claims, "Login");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Personel");
+            }
             return View();
         }
     }
